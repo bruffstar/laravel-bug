9fc2b9e76bdaefc88c9f78311d710377d9721cb7
Brian Laughlan <info@bruffstar.com>
2021-06-18 01:25:12 +0200

    Add bug page

---
+++ resources/views/bug.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel Bug</title>
+
+    <style>
+        body {
+            font-size: 20px;
+            padding: 60px;
+        }
+    </style>
+</head>
+<body>
+
+</body>
+</html>
